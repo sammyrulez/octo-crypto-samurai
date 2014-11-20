56de2eef4a9f4c2994355b86c6645f239b99e149
sammyrulez <sammyrulez@gmail.com>
2014-11-20 16:36:07 +0100

    jasmin config

---
+++ src/test/webapp/static/js/test.dependencies.js
@@ -0,0 +1,3 @@
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "jquery.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "bootstrap.min.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "jquery.loadTemplate.js");
