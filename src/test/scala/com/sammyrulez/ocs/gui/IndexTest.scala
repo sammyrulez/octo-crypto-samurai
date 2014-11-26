6cbe04874c1d5512e1eafb735a399530d14cead7
sammyrulez <sammyrulez@gmail.com>
2014-11-26 16:55:32 +0100

    selenium test

--- src/test/scala/com/sammyrulez/ocs/gui/IndexTest.scala
+++ src/test/scala/com/sammyrulez/ocs/gui/IndexTest.scala
@@ -1,14 +1,21 @@
 package com.sammyrulez.ocs.gui
 import org.scalatest._
 import org.scalatest.selenium.Firefox
+import org.scalatest.selenium.HtmlUnit
 
 class IndexSpec extends FlatSpec with ShouldMatchers with Firefox {
 
   val host = "http://localhost:8080/"
 
-  "The app home page" should "have the correct title" in {
+   "The app home page" should "have the correct title" in {
     go to (host + "index.html")
-    pageTitle should be ("Starter Template for Bootstrap")
+    pageTitle should be ("BitRyu")
+    close()
+  }
+
+  "The app home page" should "have the correct exchange value" in {
+    go to (host + "index.html")
+    id("quoteValue").element.text should be ("123")
     close()
   }
 }
