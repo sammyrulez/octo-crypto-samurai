96f7698fd4ec7e84146f0ffc09f1091e8da88b85
sammyrulez <sammyrulez@gmail.com>
2014-12-05 11:28:19 +0100

    basic impl

--- build.sbt
+++ build.sbt
@@ -18,7 +18,7 @@ libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % "2.3.0",
         "org.scalatra" %% "scalatra-scalate" % "2.3.0",
         "org.scalatra" %% "scalatra-auth" % "2.3.0",
-        "org.scalatra" %% "scalatra-scalatest" % "2.3.0" % "test",
+        "org.scalatra" %% "scalatra-specs2" % "2.3.0" % "test",
         "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.1.v20140609" % "container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
--- src/test/scala/com/sammyrulez/ocs/api/OperationsTest.scala
+++ src/test/scala/com/sammyrulez/ocs/api/OperationsTest.scala
@@ -1,22 +1,51 @@
 package com.sammyrulez.ocs.api
-import org.scalatra.test.scalatest._
-import org.scalatest.FunSuiteLike
+
+import org.scalatra.test.specs2._
+import org.specs2.mock.Mockito
+import org.mockito.Matchers._  
+import org.specs2.mutable._
+
+import javax.servlet.http._
+import javax.servlet._
+import org.scalatra.auth.Scentry
 import net.iharder.Base64
+import org.scalatra.ScalatraFilter
 
-class OperationsServletTests extends ScalatraSuite with FunSuiteLike {
+class OperationsServletTests extends MutableScalatraSpec with Mockito {    
+        addServlet(classOf[Operations], "/*")    
 
-  addServlet(classOf[Operations], "/*")
+        /*val basicCredentials = "Basic " + Base64.encodeBytes("scalatra:scalatra")   
+        val authHeader = Map("Authorization" -> basicCredentials) */  
+            var invalidateCalled = false
+            val context = new ScalatraFilter {
+                          private[this] val sessionMap = scala.collection.mutable.HashMap[String, Any](Scentry.scentryAuthKey -> "6789")
+                          val mockSession = smartMock[HttpSession]
+                          override def session(implicit request: HttpServletRequest) = mockSession
+                          mockSession.getAttribute(anyString) answers { k => sessionMap.getOrElse(k.asInstanceOf[String], null).asInstanceOf[AnyRef] }
+                          mockSession.setAttribute(anyString, anyObject) answers { (kv, wtfIsThis) =>
+                            val Array(k: String, v: Any) = kv
+                            sessionMap(k) = v
+                          }
+                          mockSession.invalidate() answers { k =>
+                            invalidateCalled = true
+                            sessionMap.clear()
+                          }
+            }
 
-  val basicCredentials = Base64.encodeBytes("scalatra:scalatra".getBytes())
-  val authHeader = Map("Authorization" -> basicCredentials)
+        implicit val req = mock[HttpServletRequest]
 
+        implicit val res = mock[HttpServletResponse].smart 
+ 
+        val mockedScentry = mock[Scentry[User]]
+        mockedScentry.isAuthenticated returns true
+        mockedScentry.authenticate returns new User("scalatra")
 
-
-  test("Post to deposit") {
-    post("/deposit", authHeader) {
-      status should equal (200)
-      body should include ("success")
-    }
-  }
-}
+       "POST / on deposit" should {
+            "return status 200" in {
+             post("/deposit") {
+                status must_== 200
+              }
+            }
+          }
+ }
 
